c158990126bf1fb2cff6ccf65c2d3f95a2920ee9
Thi Nguyen <tsokye346@gmail.com>
2019-10-10 21:48:33 -0700

    fixes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {    
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))});
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))})};
 
 app.post("/users", function(req, res) {
     console.log(req.body);
